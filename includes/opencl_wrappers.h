727c74c248e40d9eddce3039e7e880061f6345d0
Judah Jacobson <judah.jacobson@gmail.com>
2009-12-25 06:53:29 +0000

    Wrapper functions aren't used anymore.

--- includes/opencl_wrappers.h
+++ includes/opencl_wrappers.h
@@ -1,7 +1,1 @@
 #include <OpenCL/OpenCL.h>
-
-cl_int clGetDeviceInfoPtr (cl_device_id* device,
-                        cl_device_info param_name,
-                        size_t param_value_size,
-                        void *param_value,
-                        size_t *param_value_size_ret);
