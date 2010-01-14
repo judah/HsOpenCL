0d92c1bd26b4fe862723d69ae0e46df8983f3588
Judah Jacobson <judah.jacobson@gmail.com>
2010-01-14 08:35:27 +0000

    Clean up the Transpose example.

--- tests/bench/Transpose.hs
+++ tests/bench/Transpose.hs
@@ -1,53 +1,43 @@
+{-# LANGUAGE Rank2Types #-}
 module Main where
 
-import System.Time
-import Criterion.Main
+-- Taken from the NVIDIA OpenCL example code.
+-- To run:
+-- ghc --make -O2 Transpose.hs
+-- ./Transpose
 
-import OpenCL
-import OpenCL.Simple hiding (KernelArg)
-import qualified Data.ByteString as B
-import System.Random
+import System.HsOpenCL
+import System.HsOpenCL.Instances.CArray
+import ProfCommand
 import Control.Monad
-import Foreign
+import Text.Printf
+import Data.Array.CArray
 
 sizeX = 256
 sizeY = 4096
 blockDim = 16
 size2 = sizeX*sizeY
 
+declareKernelsFromFile "Transpose" "transpose.cl"
 
-main = do
-    source <- B.readFile "transpose.cl"
-    p <- newSimpleProgram DeviceTypeGPU [source]
-    ks <- createKernelsInProgram (simpleProgram p)
-    -- allocate host memory:
-    allocaArray (sizeX*sizeY) $ \h_idata -> do
-    -- fill with random data:
-    forM_ [0..sizeX*sizeY-1] $ \i -> randomIO >>= pokeElemOff h_idata i
-    -- allocate device memeory and copy host to it:
-    withBuffer (simpleCxt p) MemReadOnly
-        (CopyHostPtr h_idata) size2 $ \d_idata -> do
-    withBuffer (simpleCxt p) MemWriteOnly NoHostPtr size2 $ \d_odata -> do
-    -- Run the tests
-    bs <- mapM (testKernel p d_idata d_odata) ks
-    defaultMain bs
-    putStrLn "Finished."
 
-testKernel :: SimpleProgram -> Buffer Float -> Buffer Float -> Kernel
-            -> IO Benchmark
-testKernel p idata odata k = do
-    putStrLn $ "----- Testing " ++ kernelFunctionName k ++ " -----"
-    setKernelArgs k args
-    -- they do some sort of rounding up for the global size,
-    -- but I think with the above dimensions we're OK.
+main = runQueueForType DeviceTypeGPU $ do
+    setProperties [QueueProfilingEnable] True
+    p <- buildTranspose ""
+
+    let mkBounds m n = ((0,0),(m-1,n-1))
+    a <- randomCArray $ mkBounds sizeX sizeY
+    aM <- newBuffer MemReadOnly NoHostPtr (sizeX*sizeY)
+    bM <- newBuffer MemWriteOnly NoHostPtr (sizeX*sizeY)
+    waitForCommand $ aM =: a
+
     let globalSize = (sizeX,sizeY)
     let localSize = Just (blockDim,blockDim)
-    return $ bench ("kernel-" ++ kernelFunctionName k)
-               $ waitForCommand (simpleQueue p)
-               $ ndRangeKernel k globalSize localSize
-  where
-    localBlock :: Local Float
-    localBlock = Local $ blockDim * (blockDim+1)
-    args = idata &: odata &: Scalar sizeX &: Scalar sizeY
-                    &: if kernelNumArgs k == 5 
-                        then [SomeArg localBlock] else []
+    let cmds = [("transpose_naive", transpose_naive p globalSize localSize
+                                            aM bM sizeX sizeY)
+               , ("transpose", transpose p globalSize localSize
+                                        aM bM sizeX sizeY (Local $ blockDim*(blockDim+1)))
+               ]
+    forM_ cmds $ \(n, k) -> do
+    putStrLn' $ printf "Running %s..." n
+    profCommand 20 k
