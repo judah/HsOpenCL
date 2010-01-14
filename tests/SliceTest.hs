941224f594f86b0fbc9e4d7477c0e5117fec5641
Judah Jacobson <judah.jacobson@gmail.com>
2010-01-14 01:18:27 +0000

    Update SliceTest.

--- tests/SliceTest.hs
+++ tests/SliceTest.hs
@@ -1,8 +1,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
-import OpenCL
-import OpenCL.Simple
+import System.HsOpenCL
 
 import Foreign
 import qualified Data.ByteString.Char8 as B
@@ -11,32 +10,29 @@ n = 16
 
 main = do
     file <- B.readFile "test_prog.cl"
-    p <- newSimpleProgram DeviceTypeGPU [file]
-    let run = waitForCommand (simpleQueue p)
     withArray [0..toEnum n-1] $ \(a::Ptr Float) -> do
     allocaArray n $ \(b::Ptr Float) -> do
     allocaArray n $ \(temp::Ptr Float) -> do
-    withBuffer (simpleCxt p) MemReadWrite NoHostPtr n $ \c -> do
-    withBuffer (simpleCxt p) MemReadWrite NoHostPtr n $ \d -> do
+    runQueueForType DeviceTypeGPU $ do
+    p <- buildProgramFromSource "" [file]
+    c <- newBuffer MemReadWrite NoHostPtr n
+    d <- newBuffer MemReadWrite NoHostPtr n
     let printAll = do
-        putStrLn "-----------"
-        putStr "a: " >> peekArray n a >>= print
-        putStr "b: " >> peekArray n b >>= print
-        run $ temp =: c
-        putStr "c: " >> peekArray n temp >>= print
-        run $ temp =: d
-        putStr "d: " >> peekArray n temp >>= print
+        liftIO $ putStrLn "-----------"
+        liftIO $ putStr "a: " >> peekArray n a >>= print
+        liftIO $ putStr "b: " >> peekArray n b >>= print
+        waitForCommand $ temp =: c
+        liftIO $ putStr "c: " >> peekArray n temp >>= print
+        waitForCommand $ temp =: d
+        liftIO $ putStr "d: " >> peekArray n temp >>= print
     printAll
-    run $ c =: a
-    run $ d =: c
-    run $ b =: d
+    mapM_ waitForCommand [c =: a, d =: c, b =: d]
     printAll
-    run $ slice 5 2 c =: a
-    run $ slice 8 3 c =: slice 4 3 d
-    run $ b =: slice 11 1 c
+    mapM_ waitForCommand [slice 5 2 c =: a, slice 8 3 c =: slice 4 3 d, 
+                            b =: slice 11 1 c]
     printAll
-    print [slice 4 16 c, slice (-3) 16 c, slice 10 10 c]
-    run $ slice 5 0 c =: slice 4 0 d
+    liftIO $ print [slice 4 16 c, slice (-3) 16 c, slice 10 10 c]
+    waitForCommand $ slice 5 0 c =: slice 4 0 d
     printAll
 
 
