c2183f1c0a08839f9a6d1ff4fa76cfff49171cb5
Judah Jacobson <judah.jacobson@gmail.com>
2011-10-03 22:40:14 +0000

    Get the "Add" test up-to-date.

--- tests/Add.hs
+++ tests/Add.hs
@@ -3,21 +3,21 @@
 -- To run, use e.g. "runghc Add.hs 1024"
 
 import System.HsOpenCL
-import System.HsOpenCL.Instances.CArray
+import qualified Data.Vector.Storable as V
+import qualified Data.Vector.Storable.Mutable as M
 import System.Environment
 import Foreign
 
-import Data.Array.CArray (listArray, elems)
-import Data.Array.IOCArray (newListArray)
 import System.IO
 import Data.List (findIndices)
+import Control.Monad (zipWithM_)
 
 declareKernels "Adder" [clProg| __kernel void
             add(__global float *a,__global float *b, __global float *answer)
             {
 	        int gid = get_global_id(0);
 	        answer[gid] = a[gid] + b[gid];
-            }|])
+            }|]
 
 main = do
     hSetBuffering stdout LineBuffering
@@ -28,22 +28,26 @@ main = do
     -- Allocate host memory; this can be either pure or impure.
     let bounds = (0,size-1)
     liftIO $ putStrLn $ "Array bounds: " ++ show bounds
-    let a = asCArray $ listArray bounds [0..n-1]
-    b <- asIOCArray $ newListArray bounds [n-1,n-2..0]
+    -- Fill the vectors with some dummy values
+    let a = V.fromList [0..n-1] -- pure vector
+    b <- liftIO $ M.new size -- mutable vector
+    liftIO $ zipWithM_ (M.write b) [0..size-1] [n-1,n-2..0]
     -- Allocate device memory:
     allocaBuffer MemReadOnly NoHostPtr size $ \aMem -> do
     allocaBuffer MemReadOnly  NoHostPtr size $ \bMem -> do
     allocaBuffer MemReadWrite NoHostPtr size $ \ansMem -> do
     -- Run the program:
     prog <- buildAdder ""
-    liftIO $ putStrLn "Running..."
-    waitForCommands [aMem =: a, bMem =: b
-                    , add prog size Nothing aMem bMem ansMem
+    let x = (aMem :: Buffer Float) =: a
+    return ()
+    waitForCommands [aMem =: a]
+    waitForCommands [bMem =: b]
+    waitForCommands [add prog size Nothing aMem bMem ansMem
                     ]
-    results <- copyToCArray bounds ansMem
+    results <- copyToVector ansMem
     liftIO $ putStrLn "Done."
     liftIO $ do
-        let es = elems results
+        let es = V.toList results
         putStrLn "First 10 results are:"
         print $ take 10 es
         let ok = (== (n-1))
